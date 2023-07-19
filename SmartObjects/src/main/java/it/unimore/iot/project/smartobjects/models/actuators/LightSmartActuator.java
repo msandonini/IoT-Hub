9a32907ba42156434235953e14fc5d8a2f568bea
Mirco Sandonini <mirco.ms16@gmail.com>
2023-07-19 21:52:58 +0200

    Finished light actuator resource (theoretically)

--- SmartObjects/src/main/java/it/unimore/iot/project/smartobjects/models/actuators/LightSmartActuator.java
+++ SmartObjects/src/main/java/it/unimore/iot/project/smartobjects/models/actuators/LightSmartActuator.java
@@ -2,7 +2,11 @@ package it.unimore.iot.project.smartobjects.models.actuators;
 
 import java.awt.*;
 
-public class LightSmartActuator extends GenericActuator<Color, Boolean> {
+public class LightSmartActuator extends GenericActuator<Color, Integer> {
+
+    public static final int STATUS_OFF = -1;
+    public static final int STATUS_DARK = 0;
+    public static final int STATUS_LIGHT = 1;
 
     public static final Color DEFAULT_COLOR = Color.WHITE;
 
@@ -20,21 +24,46 @@ public class LightSmartActuator extends GenericActuator<Color, Boolean> {
     }
 
     public void setColor(Color value) {
-        if (value.equals(Color.BLACK)) {
-            setStatus(false);
-        }
-        else {
-            setSavedValue(value);
-            setStatus(true);
+        setSavedValue(value);
+    }
+
+    public Color getColor() {
+        return this.savedValue;
+    }
+
+    public static String getStatusDescription(int status) {
+        switch (status) {
+            case STATUS_OFF -> {
+                return "Off";
+            }
+            case STATUS_DARK -> {
+                return "Dark";
+            }
+            case STATUS_LIGHT -> {
+                return "Light";
+            }
+            default -> {
+                return "Unknown";
+            }
         }
     }
 
     @Override
     public void onDataChanged(Color value) {
+        if (value == null) {
+            value = DEFAULT_COLOR;
+        }
         setSavedValue(value);
 
         if (this.isActive()) {
-            setColor(value);
+            if (this.getColor().getRed() == 0 && this.getColor().getGreen() == 0 && this.getColor().getBlue() == 0) {
+                setStatus(STATUS_DARK);
+            }
+            else {
+                setStatus(STATUS_LIGHT);
+            }
+        } else {
+            setStatus(STATUS_OFF);
         }
     }
 
--- SmartObjects/src/main/java/it/unimore/iot/project/smartobjects/resources/actuators/LightActuatorResource.java
+++ SmartObjects/src/main/java/it/unimore/iot/project/smartobjects/resources/actuators/LightActuatorResource.java
@@ -6,6 +6,7 @@ import org.eclipse.californium.core.coap.MediaTypeRegistry;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 import org.json.JSONObject;
 
+import java.awt.*;
 import java.util.Optional;
 
 public class LightActuatorResource extends CoapActuatorResource<LightSmartActuator> {
@@ -13,7 +14,7 @@ public class LightActuatorResource extends CoapActuatorResource<LightSmartActuat
     private static final Number ACTUATOR_VERSION = 0.1;
 
     public LightActuatorResource(String deviceId, String name, LightSmartActuator actuator) {
-        super(deviceId, OBJECT_TITLE, name, ACTUATOR_VERSION, actuator);
+        super(deviceId, OBJECT_TITLE, name, ACTUATOR_VERSION, actuator, false);
     }
 
     private Optional<String> getJsonResponse() {
@@ -22,8 +23,11 @@ public class LightActuatorResource extends CoapActuatorResource<LightSmartActuat
         json.put("name", String.format("%s:%s", this.deviceId, this.getName()));
         json.put("version", this.actuatorVersion);
         json.put("timestamp", System.currentTimeMillis());
-        json.put("color", this.actuator.getSavedValue().getRGB());
-        json.put("status", this.actuator.getStatus());
+        json.put("color", "#" +
+                Integer.toHexString(this.actuator.getSavedValue().getRed()) +
+                Integer.toHexString(this.actuator.getSavedValue().getGreen()) +
+                Integer.toHexString(this.actuator.getSavedValue().getBlue()));
+        json.put("status", LightSmartActuator.getStatusDescription(this.actuator.getStatus()));
 
         return Optional.of(json.toString());
     }
@@ -52,5 +56,32 @@ public class LightActuatorResource extends CoapActuatorResource<LightSmartActuat
         }
     }
 
-    // TODO: POST & PUT requests
+    @Override
+    public void handlePOST(CoapExchange exchange) {
+        try {
+            this.actuator.toggleActive();
+
+            exchange.respond(CoAP.ResponseCode.CHANGED);
+        } catch (Exception e) {
+            exchange.respond(CoAP.ResponseCode.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public void handlePUT(CoapExchange exchange) {
+        try {
+            if (exchange.getRequestPayload() != null) {
+                String payload = new String(exchange.getRequestPayload()).trim();
+
+                this.actuator.setSavedValue(Color.decode(payload));
+
+                exchange.respond(CoAP.ResponseCode.CHANGED);
+            } else {
+                exchange.respond(CoAP.ResponseCode.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            exchange.respond(CoAP.ResponseCode.INTERNAL_SERVER_ERROR);
+            e.printStackTrace();
+        }
+    }
 }
