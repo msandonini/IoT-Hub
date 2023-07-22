b7bad0b310c08c12a3987a2b7657ca2ce1d87cc9
Mirco Sandonini <mirco.ms16@gmail.com>
2023-07-22 19:03:49 +0200

    Window controller sensors and actuators

--- SmartObjects/src/main/java/it/unimore/iot/project/smartobjects/WindowControllerSmartObject.java
+++ SmartObjects/src/main/java/it/unimore/iot/project/smartobjects/WindowControllerSmartObject.java
@@ -1,4 +1,67 @@
 package it.unimore.iot.project.smartobjects;
 
-public class WindowControllerSmartObject {
+import it.unimore.iot.project.smartobjects.models.actuators.LightActivatedActuator;
+import it.unimore.iot.project.smartobjects.models.actuators.LightActivatedShutterActuator;
+import it.unimore.iot.project.smartobjects.models.sensors.LightSmartSensor;
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.server.resources.Resource;
+
+public class WindowControllerSmartObject extends CoapServer {
+    public static final int COAP_PORT = 5686;
+
+    public static final String DEVICE_ID = "iot:window";
+
+    LightSmartSensor lightSensor;
+    LightActivatedActuator lightActivatedActuator;
+    LightActivatedShutterActuator shutterActuator;
+
+    public WindowControllerSmartObject(int port) {
+        super(port);
+
+        lightSensor = new LightSmartSensor();
+        lightActivatedActuator = new LightActivatedActuator();
+        shutterActuator = new LightActivatedShutterActuator();
+
+        lightSensor.addDataListener(lightActivatedActuator);
+        lightActivatedActuator.addDataListener(shutterActuator);
+
+        lightActivatedActuator.setActive(true);
+        shutterActuator.setActive(true);
+
+        logLightSensor();
+        logLightActivatedActuator();
+        logShutterActuator();
+
+        // TODO: Make resources
+    }
+
+    private void logLightSensor() {
+        lightSensor.addDataListener(value -> System.out.println(String.format("[SENSOR][LIGHT] -> [VALUE] %f", value)));
+    }
+    private void logLightActivatedActuator() {
+        lightActivatedActuator.addDataListener(value -> System.out.println(String.format("[ACTUATOR][LIGHT] -> [VALUE] %b", value)));
+    }
+    private void logShutterActuator() {
+        shutterActuator.addDataListener(value -> System.out.println(String.format("[ACTUATOR][SHUTTERS] -> [VALUE] %d", value)));
+    }
+
+    public static void main(String[] args) {
+        WindowControllerSmartObject smartObject = new WindowControllerSmartObject(COAP_PORT);
+        smartObject.start();
+
+        smartObject.getRoot().getChildren().forEach(WindowControllerSmartObject::logResource);
+    }
+
+    private static void logResource(Resource resource) {
+        System.out.println(String.format(
+                "[RESOURCE FOUND] -> [NAME] %s [URI] %s [OBSERVABLE] %b",
+                resource.getName(),
+                resource.getURI(),
+                resource.isObservable()
+        ));
+
+        if (!resource.getURI().equals("/.well-known")) {
+            resource.getChildren().forEach(WindowControllerSmartObject::logResource);
+        }
+    }
 }
--- SmartObjects/src/main/java/it/unimore/iot/project/smartobjects/models/sensors/LightSmartSensor.java
+++ SmartObjects/src/main/java/it/unimore/iot/project/smartobjects/models/sensors/LightSmartSensor.java
@@ -3,33 +3,42 @@ package it.unimore.iot.project.smartobjects.models.sensors;
 import java.util.Random;
 
 public class LightSmartSensor extends GenericSensor<Double> {
-    public static final double SIM_SUNLIGHT_LUX = 107527.0;
-    public static final double SIM_FULL_DAYLIGHT_LUX = 10752.0;
-    public static final double SIM_OVERCAST_LUX = 1075;
-    public static final double SIM_DARK_DAY_LUX = 107;
-    public static final double SIM_TWILIGHT_LUX = 10.8;
-    public static final double SIM_DEEP_TWILIGHT_LUX = 1.08;
-    public static final double SIM_FULL_MOON_LUX = 0.108;
-    public static final double SIM_QUARTER_MOON_LUX = 0.0108;
-    public static final double SIM_STARLIGHT_LUX = 0.0011;
-    public static final double SIM_OVERCAST_NIGHT_LUX = 0.0001;
+    public static final double[] SIM_DAY_VALUES = {
+            //107527.0,   // Sunlight
+            10752.0,    // Full daylight
+            1075,       // Overcast day
+            107         // Very dark day
+    };
 
-    public static final int SIM_DAY_CYCLE_DURATION_S = 60;
-    public static final int SIM_NIGHT_CYCLE_DURATION_S = 60;
+    public static final double[] SIM_NIGHT_VALUES = {
+            0.108,      // Full moon
+            0.0108,     // Quarter moon
+            0.0011,     // Starlight
+            //0.0001      // Overcast night
+    };
+    public static final double SIM_TWILIGHT_VALUE = 10.8;
+    public static final double SIM_DEEP_TWILIGHT_VALUE = 1.08;
 
-    public static final double SIM_START_LUX = SIM_SUNLIGHT_LUX;
-    public static final double SIM_FIRST_DAY_MAX_LUX = SIM_SUNLIGHT_LUX;
-    public static final double SIM_FIRST_NIGHT_MIN_LUX = SIM_OVERCAST_NIGHT_LUX;
-    public static final int SIM_START_DIRECTION_SIGN = -1;          // +1 = Ascending lux, -1 = Descending lux
+    public static final int SIM_DAY_NIGHT_CYCLE_DURATION_S = 60;
+    public static final double SIM_FIRST_DAY_MAX_LUX = SIM_DAY_VALUES[0];
+    public static final double SIM_FIRST_DAY_TWILIGHT_LUX = SIM_TWILIGHT_VALUE;
+    public static final double SIM_FIRST_NIGHT_MIN_LUX = SIM_NIGHT_VALUES[2];
 
     private transient double simDayMaxLux;
     private transient double simNightMinLux;
-
-    private transient int simDirectionSign;
+    private transient double simTwilightLux;
 
     private transient long lastUpdateTimestamp;
 
     private transient Random random;
+
+    public LightSmartSensor() {
+        super();
+    }
+
+    public LightSmartSensor(long timerUpdatePeriod, long timerTaskDelayTime) {
+        super(timerUpdatePeriod, timerTaskDelayTime);
+    }
 
     @Override
     protected void init() {
@@ -37,14 +46,38 @@ public class LightSmartSensor extends GenericSensor<Double> {
 
         this.simDayMaxLux = SIM_FIRST_DAY_MAX_LUX;
         this.simNightMinLux = SIM_FIRST_NIGHT_MIN_LUX;
-        this.simDirectionSign = SIM_START_DIRECTION_SIGN;
+        this.simTwilightLux = SIM_FIRST_DAY_TWILIGHT_LUX;
         this.lastUpdateTimestamp = System.currentTimeMillis();
 
-        setValue(SIM_START_LUX);
+        setValue(0.0);
     }
 
     @Override
     protected void updateMeasurement() {
-        // TODO: Implement day-night cycle simulation with new day & night condition at each cycle
+        double timeElapsed = (double) (System.currentTimeMillis() - this.lastUpdateTimestamp) / 1000;
+
+        if (timeElapsed >= SIM_DAY_NIGHT_CYCLE_DURATION_S) {        // If new day
+            this.lastUpdateTimestamp = System.currentTimeMillis();
+            timeElapsed = 0;
+
+            this.simDayMaxLux = SIM_DAY_VALUES[random.nextInt(SIM_DAY_VALUES.length)];
+            this.simNightMinLux = SIM_NIGHT_VALUES[random.nextInt(SIM_NIGHT_VALUES.length)];
+            this.simTwilightLux = random.nextBoolean() ? SIM_TWILIGHT_VALUE : SIM_DEEP_TWILIGHT_VALUE;
+        }
+
+        double cyclePercentage = timeElapsed / SIM_DAY_NIGHT_CYCLE_DURATION_S;
+        double angle = cyclePercentage * Math.PI * 2;
+
+        double y = Math.sin(angle);
+
+        double light;
+
+        if (y >= 0) {    // Daytime - Positive sine
+            light = y * (this.simDayMaxLux - this.simTwilightLux) + this.simTwilightLux;
+        } else {        // Nighttime - Negative sine
+            light = (this.simTwilightLux + (y * (this.simTwilightLux - this.simNightMinLux) + this.simNightMinLux));
+        }
+
+        setValue(light);
     }
 }
